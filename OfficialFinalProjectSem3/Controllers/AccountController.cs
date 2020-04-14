e7e97ffb7ef6d3f4b1df0880567b6886b3f7caf7
duypham9669 <44285940+duypham9669@users.noreply.github.com>
2020-04-14 10:34:58 +0700

    send-Email

--- OfficialFinalProjectSem3/Controllers/AccountController.cs
+++ OfficialFinalProjectSem3/Controllers/AccountController.cs
@@ -156,14 +156,13 @@ namespace OfficialFinalProjectSem3.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    await UserManager.SendEmailAsync(user.Id, "Xác thực tài khoản của bạn", "Xin hãy click vào link: <a href=\"" + callbackUrl + "\"> here</a> để thực hiện việc xác thực");
+
+                    ViewBag.Message = "Hãy kiểm tra emaill xác thực trong email của bạn";
+                    return View(model);
                 }
                 AddErrors(result);
             }
