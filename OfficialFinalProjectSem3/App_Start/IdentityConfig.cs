f170a1229929ca5153712454fe636915bc13d3c4
minhtuan244 <hoangminhtuanfpt992@gmail.com>
2020-04-28 09:16:45 +0700

    sms

--- OfficialFinalProjectSem3/App_Start/IdentityConfig.cs
+++ OfficialFinalProjectSem3/App_Start/IdentityConfig.cs
@@ -50,6 +50,14 @@ namespace OfficialFinalProjectSem3
         public Task SendAsync(IdentityMessage message)
         {
             // Plug in your SMS service here to send a text message.
+            var soapSms = new OfficialFinalProjectSem3.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            soapSms.SendSimpleTextSMS(
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSUSERKEY"],
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSPASSWORD"],
+                message.Destination,
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSORIGINATOR"],
+                message.Body);
+            soapSms.Close();
             return Task.FromResult(0);
         }
     }
