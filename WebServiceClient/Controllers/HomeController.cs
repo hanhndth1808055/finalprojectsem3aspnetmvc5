993733c050fff65b2446a4e1d3cb8ae61d972047
namlephuong95 <namlephuong95@gmail.com>
2020-05-06 22:54:46 +0700

    web-client

---
+++ WebServiceClient/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServiceClient.Models;
+
+namespace WebServiceClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var serviceClient = new ServiceReference2.Service1Client();
+            var std = new ServiceReference2.Student() {
+                Name = "Le Phuong Nam"
+            };
+            var result = serviceClient.GetStudentData(std);
+            ViewBag.Message = "Your student name: " + result.Id;
+            return View(result);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
